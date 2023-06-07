310963d0ec8149da5a94c4195d32086b32e243ba
kushar77 <kusharora339@gmail.com>
2023-06-07 13:11:28 -0700

    fixed bug

--- src/components/flashcard/dialogContainer.jsx
+++ src/components/flashcard/dialogContainer.jsx
@@ -22,7 +22,12 @@ const FlashCardModal = (props) => {
 
     return (
         <>
-            <Button variant="gradient" style={{ marginLeft: "1em" }} size="sm" onClick={() => setVisible(true)}>Open FlashCards</Button>
+            <Button variant="gradient" style={{ marginLeft: "1em" }} size="sm" onClick={() => {
+                setId(modules[0].id);
+                setVisible(true);
+            }}>
+                Open FlashCards
+            </Button>
             <Dialog open={visible} handler={setVisible} size="xl" className="flex flex-row bg-transparent shadow-none">
                 <Card className="w-1/4 w-min-1/4 h-[70vh] rounded-lg overflow-y-auto scrollbar-thin scrollbar-thumb-rounded-xl scrollbar-thumb-blue-gray-100 shadow-none">
                     <div className="flex items-center gap-4 p-4">
@@ -44,7 +49,14 @@ const FlashCardModal = (props) => {
                         }
                     </List>
                 </Card>
-                <Card className="w-3/4 rounded-lg ml-2 items-center shadow-none">
+                {/* 
+                        Self-note: Passing key below was critical to re-render each module from the beginning!
+                        When React renders a list of components, it uses the key prop to identify each component uniquely. 
+                        When the key prop of a component changes, React considers it as a completely new instance, even if 
+                        the component type remains the same. This triggers a re-render of the component, 
+                        including its child components. 
+                 */}
+                <Card className="w-3/4 rounded-lg ml-2 items-center shadow-none" key={id}>
                         <Cards moduleId={id} />
                 </Card>
             </Dialog>
