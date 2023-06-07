152e670a5580fbf38c3a3086f15c33d3c5748812
kushar77 <kusharora339@gmail.com>
2023-06-07 13:35:14 -0700

    status bar wip

--- src/components/flashcard/flashcards.js
+++ src/components/flashcard/flashcards.js
@@ -2,6 +2,7 @@ import { ArrowLeftIcon, ArrowPathRoundedSquareIcon, ChevronLeftIcon, ChevronRigh
 import { Card, Carousel, IconButton, Typography } from "@material-tailwind/react";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import StatusBar from "./statusBar";
 
 const Cards = (props) => {
     const module = useSelector(state => state.flashcards.modules.find(module => module.id === props.moduleId));
@@ -10,7 +11,7 @@ const Cards = (props) => {
     return (
         <>
             <Carousel
-                className="bg-transparent h-[85%] w-full"
+                className="bg-transparent h-[85%] w-full border border-b-light-blue-500"
                 navigation={({ setActiveIndex, activeIndex, length }) => (
                     <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
                         {new Array(length).fill("").map((_, i) => (
@@ -72,17 +73,20 @@ const Cards = (props) => {
                     ))
                 }
             </Carousel>
-            <IconButton
-                size="md"
-                variant="text"
-                className="mt-2"
-                color="blue-gray"
-                onClick={() => { setFlip(!flip); }}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
-                </svg>
-            </IconButton>
+            <div className="w-full h-[15%] flex justify-center items-center">
+                {/* <IconButton
+                    size="md"
+                    variant="text"
+                    className="mt-2"
+                    color="blue-gray"
+                    onClick={() => { setFlip(!flip); }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                    </svg>
+                </IconButton> */}
+                <StatusBar />
+            </div>
         </>
     );
 }
