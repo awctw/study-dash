d738fb35680d8c389ffd616dd3103a5c816aa4e0
kushar77 <kusharora339@gmail.com>
2023-06-07 17:28:43 -0700

    progress bar wip

--- src/components/flashcard/flashcards.js
+++ src/components/flashcard/flashcards.js
@@ -11,14 +11,14 @@ const Cards = (props) => {
     return (
         <>
             <Carousel
-                className="bg-transparent h-[85%] w-full border border-b-light-blue-500"
+                className="bg-transparent h-[85%] w-full"
                 navigation={({ setActiveIndex, activeIndex, length }) => (
                     <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
                         {new Array(length).fill("").map((_, i) => (
                             <span
                             key={i}
                             className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                                activeIndex === i ? "bg-white w-8" : "bg-white/50 w-4"
+                                activeIndex === i ? "bg-blue-gray-700 w-8" : "bg-blue-gray-400/50 w-4"
                             }`}
                             onClick={() => setActiveIndex(i)}
                             />
@@ -74,18 +74,7 @@ const Cards = (props) => {
                 }
             </Carousel>
             <div className="w-full h-[15%] flex justify-center items-center">
-                {/* <IconButton
-                    size="md"
-                    variant="text"
-                    className="mt-2"
-                    color="blue-gray"
-                    onClick={() => { setFlip(!flip); }}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
-                    </svg>
-                </IconButton> */}
-                <StatusBar />
+                <StatusBar setFlip={setFlip} flip={flip} />
             </div>
         </>
     );
--- src/components/flashcard/statusBar.js
+++ src/components/flashcard/statusBar.js
@@ -1,55 +1,20 @@
-import { Button, IconButton, MobileNav, Navbar, Typography } from "@material-tailwind/react";
+import { Button, IconButton, MobileNav, Navbar, Progress, Typography } from "@material-tailwind/react";
 import React, { useState } from "react";
 
 const StatusBar = (props) => {
     const [openNav, setOpenNav] = useState(false);
 
     const navList = (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-            >
-            <a href="#" className="flex items-center">
-                Pages
-            </a>
-            </Typography>
-            <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-            >
-            <a href="#" className="flex items-center">
-                Account
-            </a>
-            </Typography>
-            <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-            >
-            <a href="#" className="flex items-center">
-                Blocks
-            </a>
-            </Typography>
-            <Typography
-            as="li"
-            variant="small"
-            color="blue-gray"
-            className="p-1 font-normal"
-            >
-            <a href="#" className="flex items-center">
-                Docs
-            </a>
-            </Typography>
-        </ul>
+        <div className="w-full">
+            <div className="flex items-center justify-between gap-10">
+                <Typography variant="small" color="gray">progress</Typography>
+                <Typography variant="small" color="gray">{50}%</Typography>
+            </div>
+            <Progress value={50} size="sm" color="blue-gray" />
+        </div>
     );
     return (
-        <Navbar className="mx-auto py-2 px-4 mx-2 shadow-xl">
+        <Navbar className="mx-auto mb-1 py-2 px-4 mx-2 drop-shadow-2xl border border-purple-theme">
             <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
                 <Typography
                 as="a"
@@ -58,10 +23,17 @@ const StatusBar = (props) => {
                 >
                     Material Tailwind
                 </Typography>
-                <div className="hidden lg:block">{navList}</div>
-                <Button variant="gradient" size="sm" className="hidden lg:inline-block">
-                    <span>Buy Now</span>
-                </Button>
+                <div className="w-1/3 hidden lg:block">{navList}</div>
+                <IconButton
+                    size="md"
+                    variant="text"
+                    color="blue-gray"
+                    onClick={() => { props.setFlip(!props.flip); }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+                    </svg>
+                </IconButton>
             </div>
         </Navbar>
     );
