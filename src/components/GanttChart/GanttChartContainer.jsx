4d30174bf26b5873cb49b70bc4719cf44c38d6ad
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2023-06-09 10:23:54 -0700

    Add box shadow to GanttChartContainer

--- src/components/GanttChart/GanttChartContainer.jsx
+++ src/components/GanttChart/GanttChartContainer.jsx
@@ -1,9 +1,13 @@
 import GanttChart from "./GanttChart";
 
 const GanttChartContainer = () => {
-    return <div className='gantt-chart'>
-        <GanttChart />
-    </div>
+    return (
+        <div className='bg-clip-border rounded-xl bg-white shadow-lg'>
+            <div className='gantt-chart'>
+                <GanttChart />
+            </div>
+        </div>
+    );
 }
 
 export default GanttChartContainer;
