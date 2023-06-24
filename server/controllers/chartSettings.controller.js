930c00c241c657c2c7c780735997b010ed63d64c
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2023-06-24 06:34:43 -0700

    Add default ChartSettings to server

--- server/controllers/chartSettings.controller.js
+++ server/controllers/chartSettings.controller.js
@@ -1,10 +1,26 @@
 const ChartSettings= require('../models/chartSettings.model');
 
+const defaultChartSettings = {
+    axisScale: 24,
+    categoryColors: [
+        { category: "Biology", color: "#42f560" },
+        { category: "Chemistry", color: "#d742f5" },
+        { category: "Computer Science", color: "#55515e" },
+        { category: "Language Arts", color: "#b726c7" },
+        { category: "Math", color: "#2e26c7" },
+        { category: "Musical Art", color: "#bfc726" },
+        { category: "Physics", color: "#000000" },
+        { category: "Sports", color: "#ff001e" },
+        { category: "Visual Arts", color: "#ff7b00" },
+        { category: "Work", color: "#00eaff" },
+    ],
+}
+
 const getChartSettings = async (req, res, next) => {
     await ChartSettings.findOne({ userEmail: req.params.userEmail })
         .then((result) => {
             if (!result) {
-                res.status(404).send("User ChartSettings not found!");
+                res.status(200).send(defaultChartSettings);
             } else {
                 res.status(200).send(result);
             }
