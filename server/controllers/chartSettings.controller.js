b7840abf0bbeb86ae210557c8b2cd5e412f9ab1a
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2023-07-15 05:17:30 -0700

    Fix ChartSettings updating when it shouldn't

--- server/controllers/chartSettings.controller.js
+++ server/controllers/chartSettings.controller.js
@@ -2,44 +2,41 @@ const ChartSettings= require('../models/chartSettings.model');
 const Category = require("../models/Category.model");
 
 const getChartSettings = async (req, res, next) => {
-    await ChartSettings.findOne({ userID: req.params.userID })
-        .then(async (result) => {
-            if (!result) {
-                let categoryColors = [];
-                for await (const category of await Category
-                    .find({userID: req.params.userID})) {
-                    categoryColors.push({
+    try {
+        const result = await ChartSettings.findOne({ userID: req.params.userID });
+        if (!result) {
+            let categoryColors = [];
+            for (const category of await Category.find({userID: req.params.userID})) {
+                categoryColors.push({
+                    categoryID: category["_id"],
+                    category: category.category,
+                    color: '#000000'
+                })
+            }
+            const chartSettings = new ChartSettings({
+                userID: req.params.userID,
+                axisScale: 24,
+                categoryColors: categoryColors
+            });
+            await chartSettings.save()
+            res.status(200).send(chartSettings);
+        } else {
+            for (const category of await Category.find({userID: req.params.userID})) {
+                if (result.categoryColors.find(c => c.categoryID === category["_id"].toString()) === undefined) {
+                    result.categoryColors.push({
                         categoryID: category["_id"],
                         category: category.category,
                         color: '#000000'
-                    })
+                    });
                 }
-                const chartSettings = new ChartSettings({
-                    userID: req.params.userID,
-                    axisScale: 24,
-                    categoryColors: categoryColors
-                });
-                await chartSettings.save()
-                res.status(200).send(chartSettings);
-            } else {
-                for await (const category of await Category
-                    .find({userID: req.params.userID})) {
-                    if (result.categoryColors.find(c => c.categoryID === category["_id"])) {
-                        result.categoryColors.push({
-                            categoryID: category["_id"],
-                            category: category.category,
-                            color: '#000000'
-                        });
-                    }
-                }
-                await result.save();
-                res.status(200).send(result);
             }
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).send(err);
-        });
+            await result.save();
+            res.status(200).send(result);
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
 }
 
 const putChartSettings = async (req, res, next) => {
