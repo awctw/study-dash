93bc8569f281eb6db64f846c72349a60911af00b
Kenneth <56493743+leungkenneth@users.noreply.github.com>
2023-08-06 16:17:31 -0700

    Fix LoginPage path console errors

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -88,20 +88,20 @@ const LoginPage = () => {
             <path
               d="M5 16.4754C11.7688 27.4499 21.2452 57.3224 5 89.0164"
               stroke="currentColor"
-              stroke-width="10"
-              stroke-linecap="round"
+              strokeWidth="10"
+              strokeLinecap="round"
             />
             <path
               d="M33.6761 112.104C44.6984 98.1239 74.2618 57.6776 83.4821 5"
               stroke="currentColor"
-              stroke-width="10"
-              stroke-linecap="round"
+              strokeWidth="10"
+              strokeLinecap="round"
             />
             <path
               d="M50.5525 130C68.2064 127.495 110.731 117.541 116 78.0874"
               stroke="currentColor"
-              stroke-width="10"
-              stroke-linecap="round"
+              strokeWidth="10"
+              strokeLinecap="round"
             />
           </svg>
         </div>
@@ -118,8 +118,8 @@ const LoginPage = () => {
             <path
               d="M4 82.4591C54.7956 92.8751 30.9771 162.782 68.2065 181.385C112.642 203.59 127.943 78.57 122.161 25.5053C120.504 2.2376 93.4028 -8.11128 89.7468 25.5053C85.8633 61.2125 130.186 199.678 180.982 146.248L214.898 107.02C224.322 95.4118 242.9 79.2851 258.6 107.02C274.299 134.754 299.315 125.589 309.861 117.539L343 93.4426"
               stroke="currentColor"
-              stroke-width="7"
-              stroke-linecap="round"
+              strokeWidth="7"
+              strokeLinecap="round"
             />
           </svg>
         </div>
